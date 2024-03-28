867712fff2e69a436601e17b18591fbdc2495b87
DOK <148057045+Pongnakorn16@users.noreply.github.com>
2024-03-29 00:27:40 +0700

    asdasdasdasd

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class Constants {
 //   public readonly API_ENDPOINT: string = 'http://202.28.34.197/tripbooking';
   // public readonly API_ENDPOINT: string = 'https://cslab.it.msu.ac.th/tripbooking';
-  public readonly API_ENDPOINT: string = 'http://localhost:3004';
-  // public readonly API_ENDPOINT: string = 'https://node-express-1-lt1s.onrender.com';
+  // public readonly API_ENDPOINT: string = 'http://localhost:3004';
+  public readonly API_ENDPOINT: string = 'https://node-express-1-lt1s.onrender.com';
   
 }
